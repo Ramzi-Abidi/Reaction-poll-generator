9bf90d1d56ccc8cc29851a5ad650956f933eb438
Ramzi-Abidi <ramziinfo2001@gmail.com>
2024-04-23 22:40:33 +0100

    feat/text-image-polls: add paragraph to describe the app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 type headerProp = {
     pollIcon: string;
 };
 
-export const Header = (prop: headerProp) => {
+export const Header: React.FC<headerProp> = (prop: headerProp) => {
     const { pollIcon } = prop;
+    const location = useLocation();
 
     return (
         <>
@@ -26,16 +27,40 @@ export const Header = (prop: headerProp) => {
                     </a>
                 </div>
             </div>
+            <div className="reaction-poll-description">
+                <p>
+                    Create LinkedIn polls effortlessly! ✅<br /> Our app allows you to
+                    create custom <b>Text/Images polls</b> to gather feedback,
+                    understand trends, or initiate discussions within your
+                    network. 📊🗳️
+                </p>
+            </div>
 
             <div className="links">
                 <nav>
                     <ul>
-                        <li className="first-link">
+                        <li
+                            className="first-link"
+                            style={{
+                                background:
+                                    location.pathname === "/"
+                                        ? "#c9e3fa54"
+                                        : "transparent",
+                            }}
+                        >
                             <Link to="/" style={{ padding: 5 }}>
                                 Text poll
                             </Link>
                         </li>
-                        <li className="second-link">
+                        <li
+                            className="second-link"
+                            style={{
+                                background:
+                                    location.pathname === "/image-poll"
+                                        ? "#c9e3fa54"
+                                        : "transparent",
+                            }}
+                        >
                             <Link to="/image-poll" style={{ padding: 5 }}>
                                 Images poll
                             </Link>
