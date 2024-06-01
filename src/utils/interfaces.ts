154af66db6fa47fb9eada82c145553b97f4bc7db
Ramzi-Abidi <ramziinfo2001@gmail.com>
2024-06-01 15:01:15 +0100

    fixes

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,17 @@
+export interface ArrowProps {
+    iconType: string;
+}
+
+export type icon = {
+    img: string;
+    title: string;
+    type: string;
+};
+
+export type iconStates = {
+    likeState: string;
+    loveState: string;
+    supportState: string;
+    curiousState: string;
+    insightState: string;
+};
